e2af244ee259b74c3169775df19a81fbee90ff87
Allan Zhou <allan.zhou@berkeley.edu>
2016-02-08 20:03:03 -0800

    Simple planner class

---
+++ SimpleUtils.py
@@ -0,0 +1,101 @@
+import numpy
+import openravepy
+
+class SimplePlanner:
+    def __init__(self, env, planner='birrt'):
+        self.env = env
+        self.planner = openravepy.RaveCreatePlanner(self.env, planner)
+
+    def PlanToConfiguration(self, robot, goal, time_traj=True, **kw_args):
+        """
+        Plan to a desired configuration with OpenRAVE.
+        Adds timing data if time_traj is true.
+        @param robot the robot whose active DOFs will be used
+        @param goal the desired robot joint configuration
+        @param time_traj declares whether the trajectory should have timestamps
+        @return traj a trajectory from current configuration to specified goal
+        """
+
+        traj = self._Plan(robot, goal, **kw_args)
+        if time_traj:
+            with self.env:
+                openravepy.planningutils.RetimeActiveDOFTrajectory(traj, robot)
+        return traj
+
+    def GenerateRetimedTrajectory(self, robot, traj, f=lambda t: t, num_wp_new=None):
+        '''Retimes input trajectory for robot according to the given function f.
+
+        Here f maps new trajectory time to source trajectory time, where inputs and
+        outputs are proportions (ie, they are in the range [0, 1]).
+
+        @param robot the robot
+        @param traj the source trajectory
+        @param f the function mapping source trajectory time to output traj time
+        @param num_wp_new the number of waypoints in output traj
+        @return new_traj the retimed trajectory
+        '''
+        num_wp_src = traj.GetNumWaypoints()
+        num_wp_new = num_wp_src if not num_wp_new else num_wp_new
+        dur, spec = traj.GetDuration(), traj.GetConfigurationSpecification()
+        # Make sure that input function does not change total trajectory duration
+        assert abs(f(1) - 1.0) < .001
+        assert abs(f(0) - 0.0) < .001
+
+        new_traj = openravepy.RaveCreateTrajectory(robot.GetEnv(), '')
+        new_traj.Init(spec)
+
+        t_interval = dur / (num_wp_new - 1)
+        beginning, end = traj.GetWaypoint(0), traj.GetWaypoint(num_wp_src - 1)
+        # The start waypoint remains the same
+        new_traj.Insert(0, beginning)
+        for i in range(1, num_wp_new-1):
+            wp = traj.Sample(dur * f(float(i) / num_wp_new))
+            spec.InsertDeltaTime(wp, t_interval)
+            new_traj.Insert(i, wp)
+        # End waypoint remains the same
+        spec.InsertDeltaTime(end, t_interval)
+        new_traj.Insert(num_wp_new - 1, end)
+        # Retime velocities
+        retime_result = openravepy.planningutils.RetimeTrajectory(new_traj, hastimestamps=True)
+        return new_traj
+
+    def _Plan(self, robot, goals, maxiter=500, continue_planner=False,
+              or_args=None, **kw_args):
+
+        # Get rid of default postprocessing
+        extraParams =  '<_postprocessing planner=""><_nmaxiterations>0</_nmaxiterations></_postprocessing>'
+        # Maximum planner iterations
+        extraParams += '<_nmaxiterations>{:d}</_nmaxiterations>'.format(maxiter)
+
+        if or_args is not None:
+            for key, value in or_args.iteritems():
+                extraParams += '<{k:s}>{v:s}</{k:s}>'.format(k=str(key),
+                                                             v=str(value))
+
+        params = openravepy.Planner.PlannerParameters()
+        params.SetRobotActiveJoints(robot)
+        params.SetGoalConfig(goals)
+        params.SetExtraParameters(extraParams)
+
+        traj = openravepy.RaveCreateTrajectory(self.env, 'GenericTrajectory')
+
+        try:
+            self.env.Lock()
+
+            # Plan.
+            if (not continue_planner) or not self.setup:
+                self.planner.InitPlan(robot, params)
+                self.setup = True
+
+            status = self.planner.PlanPath(traj, releasegil=True)
+            from openravepy import PlannerStatus
+            if status not in [PlannerStatus.HasSolution,
+                              PlannerStatus.InterruptedWithSolution]:
+                raise PlanningError('Planner returned with status {:s}.'
+                                    .format(str(status)))
+        except Exception as e:
+            raise PlanningError('Planning failed with error: {:s}'.format(e))
+        finally:
+            self.env.Unlock()
+
+        return traj
