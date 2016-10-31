80abdbb916e439b4d58c55b07ae3ee374d7ab890
Isabel <isabelp@berkeley.edu>
2016-10-30 23:15:26 -0400

    Moved the Action class to the CameraRecorder.py file

--- CameraRecorder.py
+++ CameraRecorder.py
@@ -10,7 +10,13 @@ from OrScreenshotter import OrScreenshotter
 
 class CameraRecorder:
     """
-    A class that takes pictures of a 
+    A class that records frames of a robot performing various actions, and dumps
+    them into a directory (the frames are pictures, but they can be strung
+    together to make videos).
+    Has the advantage that the camera can also move in a trajectory.
+    The main functionality is in the CaptureFrames function. However, you must
+    call functions that set the robot and camera actions before calling
+    CaptureFrames.
     """
     def __init__(self, env, robot, time_interval=0.041, actions=[],
                  video_size=[640, 480],
@@ -229,3 +235,58 @@ class CameraRecorder:
         for action in self.actions:
             if action.name == "traj":
                 return action.trajectory.GetWaypoint(0)
+
+class Action:
+    """
+    An action that the robot should perform. Can have one of four types:
+    "traj", with the argument list consisting of the trajectory to execute
+    "wait", with the argument list consisting of the duration of the wait
+    "grab", with the argument list consisting of the object to grab
+    "release", with the argument list consisting of the object to release
+    "settransform", with the argument list consisting of the object to move,
+        and the transform it is being set to.
+    We define the terminology that "traj" and "wait" are timed actions (as
+        they take time), and the rest are auxilliary actions.
+
+    The Action is the format in which the CameraRecorder takes robot actions it
+    should record.
+    """
+    def __init__(self, name, robot=None, trajectory=None, time=0, kinbody=None,
+                 transform=None):
+        assert name in ["traj", "wait", "grab", "release", "settransform"],\
+            "please specify a valid action name"
+        self.name = name
+        if name == "traj":
+            assert trajectory is not None, "trajectory argument must be non\
+                                            none for traj actions"
+            self.trajectory = trajectory
+        if name == "wait":
+            assert time > 0, "wait times must be positive"
+            self.time = time
+        if name == "grab":
+            assert kinbody is not None, "kinbody argument must be non-none for\
+                                         grab actions"
+            self.kinbody = kinbody
+        if name == "release":
+            assert kinbody is not None, "kinbody argument must be non-none for\
+                                         release actions"
+            self.kinbody = kinbody
+        if name == "settransform":
+            assert kinbody is not None and transform is not None,\
+                "kinbody and transform arguments must be non-none for settransform"
+            self.kinbody = kinbody
+            self.transform = transform
+
+    def is_aux_action(self):
+        return self.name == "grab" or\
+               self.name == "release" or\
+               self.name == "settransform"
+
+    def get_duration(self):
+        if self.is_aux_action():
+            return -1
+        if self.name == "traj":
+            return self.trajectory.GetDuration()
+        if self.name == "wait":
+            return self.time
+
